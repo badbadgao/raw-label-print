d6a33ee3a1781c62c79a1e35f9c0764a1e652d06
Wei Gao <wei.gao@kiwiplan.co.nz>
2019-10-07 21:00:46 +0100

    add router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
 
-function App() {
+
+const Home = () => (
+  <div>
+    Home
+  </div>
+);
+
+const Public = ({match}) => (
+  <div>Public{match.params.name}</div>
+);
+
+const User = () => (
+  <div>User</div>
+);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/public/:name" component={Public} />
+      <Route path="/login" component={User} />
+    </Router>
   );
 }
 
